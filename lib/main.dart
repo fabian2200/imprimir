bc4c379f45ac6fdfbda2fceecd068d6a004692f4
Fabian Quintero <grovveip@gmail.com>
2024-07-12 16:45:04 -0500

    prueba xprint

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:bluetooth_print/bluetooth_print.dart';
-import 'package:bluetooth_print/bluetooth_print_model.dart';
+import 'package:bluetooth_x_print/bluetooth_print.dart';
+import 'package:bluetooth_x_print/bluetooth_print_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -17,7 +18,7 @@ class _MyAppState extends State<MyApp> {
   BluetoothPrint bluetoothPrint = BluetoothPrint.instance;
 
   bool _connected = false;
-  BluetoothDevice? _device;
+  BluetoothDevice _device;
   String tips = 'no device connect';
 
   @override
@@ -31,10 +32,10 @@ class _MyAppState extends State<MyApp> {
   Future<void> initBluetooth() async {
     bluetoothPrint.startScan(timeout: Duration(seconds: 4));
 
-    bool isConnected=await bluetoothPrint.isConnected??false;
+    bool isConnected = await bluetoothPrint.isConnected;
 
     bluetoothPrint.state.listen((state) {
-      print('******************* cur device status: $state');
+      print('cur device status: $state');
 
       switch (state) {
         case BluetoothPrint.CONNECTED:
@@ -68,7 +69,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
-            title: const Text('BluetoothPrint example app'),
+            title: const Text('BluetoothXPrint example app'),
           ),
           body: RefreshIndicator(
             onRefresh: () =>
@@ -90,15 +91,15 @@ class _MyAppState extends State<MyApp> {
                     stream: bluetoothPrint.scanResults,
                     initialData: [],
                     builder: (c, snapshot) => Column(
-                      children: snapshot.data!.map((d) => ListTile(
+                      children: snapshot.data.map((d) => ListTile(
                         title: Text(d.name??''),
-                        subtitle: Text(d.address??''),
+                        subtitle: Text(d.address),
                         onTap: () async {
                           setState(() {
                             _device = d;
                           });
                         },
-                        trailing: _device!=null && _device!.address == d.address?Icon(
+                        trailing: _device!=null && _device.address == d.address?Icon(
                           Icons.check,
                           color: Colors.green,
                         ):null,
@@ -116,11 +117,8 @@ class _MyAppState extends State<MyApp> {
                             OutlinedButton(
                               child: Text('connect'),
                               onPressed:  _connected?null:() async {
-                                if(_device!=null && _device!.address !=null){
-                                  setState(() {
-                                    tips = 'connecting...';
-                                  });
-                                  await bluetoothPrint.connect(_device!);
+                                if(_device!=null && _device.address !=null){
+                                  await bluetoothPrint.connect(_device);
                                 }else{
                                   setState(() {
                                     tips = 'please select device';
@@ -133,55 +131,26 @@ class _MyAppState extends State<MyApp> {
                             OutlinedButton(
                               child: Text('disconnect'),
                               onPressed:  _connected?() async {
-                                setState(() {
-                                  tips = 'disconnecting...';
-                                });
                                 await bluetoothPrint.disconnect();
                               }:null,
                             ),
                           ],
                         ),
-                        Divider(),
-                        OutlinedButton(
-                          child: Text('print receipt(esc)'),
-                          onPressed:  _connected?() async {
-                            Map<String, dynamic> config = Map();
-
-                            List<LineText> list = [];
-
-                            list.add(LineText(type: LineText.TYPE_TEXT, content: '**********************************************', weight: 1, align: LineText.ALIGN_CENTER,linefeed: 1));
-                            list.add(LineText(type: LineText.TYPE_TEXT, content: '打印单据头', weight: 1, align: LineText.ALIGN_CENTER, fontZoom: 2, linefeed: 1));
-                            list.add(LineText(linefeed: 1));
-
-                            list.add(LineText(type: LineText.TYPE_TEXT, content: '----------------------明细---------------------', weight: 1, align: LineText.ALIGN_CENTER, linefeed: 1));
-                            list.add(LineText(type: LineText.TYPE_TEXT, content: '物资名称规格型号', weight: 1, align: LineText.ALIGN_LEFT, x: 0,relativeX: 0, linefeed: 0));
-                            list.add(LineText(type: LineText.TYPE_TEXT, content: '单位', weight: 1, align: LineText.ALIGN_LEFT, x: 350, relativeX: 0, linefeed: 0));
-                            list.add(LineText(type: LineText.TYPE_TEXT, content: '数量', weight: 1, align: LineText.ALIGN_LEFT, x: 500, relativeX: 0, linefeed: 1));
-
-                            list.add(LineText(type: LineText.TYPE_TEXT, content: '混凝土C30', align: LineText.ALIGN_LEFT, x: 0,relativeX: 0, linefeed: 0));
-                            list.add(LineText(type: LineText.TYPE_TEXT, content: '吨', align: LineText.ALIGN_LEFT, x: 350, relativeX: 0, linefeed: 0));
-                            list.add(LineText(type: LineText.TYPE_TEXT, content: '12.0', align: LineText.ALIGN_LEFT, x: 500, relativeX: 0, linefeed: 1));
-
-                            list.add(LineText(type: LineText.TYPE_TEXT, content: '**********************************************', weight: 1, align: LineText.ALIGN_CENTER,linefeed: 1));
-                            list.add(LineText(linefeed: 1));;
-
-                            await bluetoothPrint.printReceipt(config, list);
-                          }:null,
-                        ),
                         OutlinedButton(
                           child: Text('print label(tsc)'),
                           onPressed:  _connected?() async {
                             Map<String, dynamic> config = Map();
-                            config['width'] = 32; // 标签宽度，单位mm
-                            config['height'] = 25; // 标签高度，单位mm
+                            config['width'] = 50; // 标签宽度，单位mm
+                            config['height'] = 30; // 标签高度，单位mm
                             config['gap'] = 2; // 标签间隔，单位mm
 
                             // x、y坐标位置，单位dpi，1mm=8dpi
                             List<LineText> list = [];
-                            list.add(LineText(type: LineText.TYPE_BARCODE, x:8, y:8, content: 'qrcode i\n'));
+                            list.add(LineText(type: LineText.TYPE_TEXT, x:10, y:10, content: 'A Title'));
+                            list.add(LineText(type: LineText.TYPE_TEXT, x:10, y:40, content: 'this is content'));
+                            list.add(LineText(type: LineText.TYPE_QRCODE, x:20, y:5, content: 'Content was Here'));
 
-                            var respuesta = await bluetoothPrint.printLabel(config, list);
-                            print("Respuesta"+respuesta.toString());
+                            await bluetoothPrint.printLabel(config, list);
                           }:null,
                         ),
                         OutlinedButton(
@@ -201,7 +170,7 @@ class _MyAppState extends State<MyApp> {
           stream: bluetoothPrint.isScanning,
           initialData: false,
           builder: (c, snapshot) {
-            if (snapshot.data == true) {
+            if (snapshot.data) {
               return FloatingActionButton(
                 child: Icon(Icons.stop),
                 onPressed: () => bluetoothPrint.stopScan(),
